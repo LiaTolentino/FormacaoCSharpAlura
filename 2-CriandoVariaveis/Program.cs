094dfc2a9ef0845da843f0d9a8ad7a542ecc7897
Natalia Tolentino <naattolentino@gmail.com>
2023-02-09 21:04:14 -0300

    c# lessons

--- 2-CriandoVariaveis/Program.cs
+++ 2-CriandoVariaveis/Program.cs
@@ -11,6 +11,13 @@ namespace _2_CriandoVariaveis
         static void Main(string[] args)
         {
             int idade;
+
+            idade = 2023 - 1994;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Pressione enter para fechar ...");
+            Console.ReadLine();
         }
     }
 }
